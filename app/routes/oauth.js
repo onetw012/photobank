df40c4da5412a44e909c92cccddb4af92276fd3d
onetw012 <lukashenko.a.one@gmail.com>
2015-07-16 21:26:02 +0300

    micro fix

--- app/routes/oauth.js
+++ app/routes/oauth.js
@@ -2,6 +2,6 @@ module.exports = {
 	flickr: {
 		clientID: '8066d9b1ddc440796c8c35c642410bb0',
 		clientSecret: '3996299b134aba38',
-		callbackURL: 'https://photobank.herokuapp.com//auth/flickr/callback'
+		callbackURL: 'https://photobank.herokuapp.com/auth/flickr/callback'
 	}
 };
